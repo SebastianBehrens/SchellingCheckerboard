1abfcacb0e6fd974fae135be9bb3d39fe0b2f077
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2022-06-24 17:08:38 +0200

    added tracker of "similarity" and added a different version of the relocation mechanism

--- SimulatingSchellingsCheckerboardModel.R
+++ SimulatingSchellingsCheckerboardModel.R
@@ -72,6 +72,21 @@ someoneuncontent <- function(grid){
 }
 someoneuncontent(board)
 
+avg_similarity <- function(grid){
+  neighbour_report <- matrix(ncol = 3)
+  for(m_cycle in 1:grid_size){
+    for(n_cycle in 1:grid_size){
+      if(board[m_cycle, n_cycle] != 0){
+      aux_nbs_same_as_self <- checkneighbours(board, c(m_cycle, n_cycle))
+      neighbour_report <- rbind(neighbour_report, c(m_cycle, n_cycle, aux_nbs_same_as_self))
+      }
+    }
+  }
+  neighbour_report <- neighbour_report[-1, ]
+  out <- neighbour_report[, 3] %>% mean()
+  return(out)
+}
+
 findemptyspots <- function(grid){
   emptyspots_report <- matrix(ncol = 3) # third col: who could move there: 0 if both could move there
   for(m in 1:grid_size){
@@ -90,6 +105,9 @@ findemptyspots <- function(grid){
   return(emptyspots_report)
 }
 
+grid_size <- 10
+board <- matrix(sample(c(0,1,-1), grid_size^2, replace = T), nrow = grid_size, ncol = grid_size)
+tracked_avg_similarity <- c()
 while(someoneuncontent(board)){
   nb_rep <- nb_report(board)
   positions_to_move <- nb_rep[nb_rep[,3] <= 0.5, ]
@@ -102,15 +120,18 @@ while(someoneuncontent(board)){
     aux_mover_n <- aux_mover[2]
     # find spots
     emptyspots <- findemptyspots(board)
-    aux_spotsavailable_helper1 <- emptyspots[, 3] == board[aux_mover_m, aux_mover_n] 
-    aux_spotsavailable_helper2 <- emptyspots[, 3] == 0
-    aux_spotsavailable <- aux_spotsavailable_helper1 | aux_spotsavailable_helper2
+    # version with movement to places where the mover will be content
+    # aux_spotsavailable_helper1 <- emptyspots[, 3] == board[aux_mover_m, aux_mover_n] 
+    # aux_spotsavailable_helper2 <- emptyspots[, 3] == 0
+    # aux_spotsavailable <- aux_spotsavailable_helper1 | aux_spotsavailable_helper2
+    # version with movement to anywhere
+    aux_spotsavailable <- emptyspots
     # relocate mover
     if(sum(aux_spotsavailable) == 0){
       print("No moves possible.")
       no_move_counter <- no_move_counter + 1
     }else{
-      aux_next_pos_ind <- sample(which(aux_spotsavailable), 1)
+      aux_next_pos_ind <- sample(nrow(aux_spotsavailable), 1)
       next_pos_m <- emptyspots[aux_next_pos_ind, 1]
       next_pos_n <- emptyspots[aux_next_pos_ind, 2]
       # emptyspots <- emptyspots[-aux_next_pos_ind, ]
@@ -119,6 +140,7 @@ while(someoneuncontent(board)){
       print(paste0("From: [", aux_mover_m, ", ", aux_mover_n, "] --- To: [", next_pos_m, ", ", next_pos_n, "]"))
       no_move_counter <- 0
     }
+    tracked_avg_similarity <- c(tracked_avg_similarity, avg_similarity(board))
     if(no_move_counter == 5){
       print("No more moves possible")
       break
@@ -126,3 +148,6 @@ while(someoneuncontent(board)){
 }
 
 nb_report(board)
+plot(1:length(tracked_avg_similarity), tracked_avg_similarity)
+
+length(tracked_avg_similarity)
